3b8c04048bc0f6493032ed576acabdfffc399915
JansonZhang1981 <tommyzjc@126.com>
2016-04-10 15:51:51 +0800

    V1

---
+++ U8SOImport/util.vb
@@ -0,0 +1,14 @@
+﻿Imports UFIDA.U8.MomServiceCommon
+Imports UFIDA.U8.U8MOMAPIFramework
+Imports UFIDA.U8.U8APIFramework
+Imports UFIDA.U8.U8APIFramework.Meta
+Imports UFIDA.U8.U8APIFramework.Parameter
+Imports MSXML2
+Imports System.Data
+Imports System.Data.OleDb
+Module util
+    Public u8login As U8Login.clsLogin
+    Public connstr As String
+    Public conn As New OleDbConnection
+
+End Module
